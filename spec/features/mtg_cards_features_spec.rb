11b9204c2f464777633817e683fcc7fafde89883
Chris Riggan <cburrows@uccs.edu>
2023-04-30 01:22:44 +0000

    updated model testing

--- spec/features/mtg_cards_features_spec.rb
+++ spec/features/mtg_cards_features_spec.rb
@@ -1,33 +1,55 @@
 require "rails_helper"
 
 RSpec.feature "MtgCards", type: :feature do
-    context "Update MtgCard" do
-        let(:mtg_card) { MtgCard.create(Name: "card name", Set: "some set", Set_Number: 1) }
-
-        before(:each) do
-          user = FactoryBot.create(:user)
-          login_as(user)
-          visit edit_mtg_card_path(mtg_card)
-        end
-   
-   
-        scenario "should be successful" do
-          within("form") do
-            fill_in "Name", with: "new name"
-          end
-          click_button "Update Mtg card"
-          expect(page).to have_content("Mtg card was successfully updated.")
-        end
-   
-   
-        scenario "should fail" do
-          within("form") do
-            fill_in "Name", with: ""
-          end
-          click_button "Update Mtg card"
-          expect(page).to have_content("Name can't be blank")
-        end
+  
+  context "Login" do
+    scenario "should sign up" do
+      visit root_path
+      click_link 'Sign up'
+      within("form") do
+        fill_in "Email", with: "testing@test.com"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Sign up"
       end
+      expect(page).to have_content("Welcome! You have signed up successfully.")
+    end
+
+    scenario "should log in" do
+      user = FactoryBot.create(:user)
+      login_as(user)
+      visit root_path
+      expect(page).to have_content("Logged in")
+    end
   end
+
+  context "Update MtgCard" do
+    let(:mtg_card) { MtgCard.create(Name: "card name", Set: "some set", Set_Number: 1) }
+
+      before(:each) do
+        user = FactoryBot.create(:user)
+        login_as(user)
+        visit edit_mtg_card_path(mtg_card)
+      end
+   
+   
+      scenario "should be successful" do
+        within("form") do
+          fill_in "Name", with: "new name"
+        end
+        click_button "Update Mtg card"
+        expect(page).to have_content("Mtg card was successfully updated.")
+      end
+   
+   
+      scenario "should fail" do
+        within("form") do
+          fill_in "Name", with: ""
+        end
+        click_button "Update Mtg card"
+        expect(page).to have_content("Name can't be blank")
+      end
+    end
+end
    
   
--- spec/models/mtg_card_spec.rb
+++ spec/models/mtg_card_spec.rb
@@ -26,10 +26,52 @@ describe "Card Attribute Requirements on Create", :type => :model do
     it "should be able to save project when have Name, set, and set number" do
         mtg_card = MtgCard.new(Name: "card name", Set: "some set", Set_Number: 1)
         expect(mtg_card.save).to eq(true)
-      end
     end
   end
-describe "Project Attribute Requirements on Edit", :type => :model do
+end
+
+describe "card Attribute Requirements on Edit", :type => :model do
+  context "validation tests" do
+
+    before (:each) do
+      @mtg_card = MtgCard.new(Name: "card name", Set: "some set", Set_Number: 1, Quantity: 1)
+    end
+
+    it "ensures the Name is present when editting card" do
+      @mtg_card.update(:Name => "")
+      expect(@mtg_card.valid?).to eq(false)
+    end
+
+    it "ensures the Name is changed when editting card" do
+      @mtg_card.update(:Name => "new name")
+      expect(@mtg_card.Name == "new name")
+    end
+
+    it "ensures the Set is present when editting card" do
+      @mtg_card.update(:Set => "")
+      expect(@mtg_card.valid?).to eq(false)
+    end
+
+    it "ensures the Set is changed when editting card" do
+      @mtg_card.update(:Set => "new set")
+      expect(@mtg_card.Set == "new set")
+    end
+    
+    it "ensures the Set number is present when editting card" do
+      @mtg_card.update(:Set_Number => "")
+      expect(@mtg_card.valid?).to eq(false)
+    end
+
+    it "ensures the set number is changed when editting card" do
+      @mtg_card.update(:Set_Number => 2)
+      expect(@mtg_card.Set_Number == 2)
+    end
+
+    it "ensures the Quantity is changed when editting a card" do
+      @mtg_card.update(:Quantity => 2)
+      expect(@mtg_card.Quantity == 2)
+    end
     
   end
+end
   
